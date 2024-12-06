66839e46076800da0a9d59c8ef342bf470960204
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2024-12-06 23:18:12 +0100

    feat: day 6 part 2

--- packages/day-6/src/functions.rs
+++ packages/day-6/src/functions.rs
@@ -1,8 +1,10 @@
+use std::collections::HashSet;
+
 pub fn is_inside_coord(x: i32, y: i32, m: usize, n: usize) -> bool {
   x >= 0 && x < (m as i32) && y >= 0 && y < (n as i32)
 }
 
-pub fn find_guard_position(grid: Vec<Vec<char>>) -> (usize, usize) {
+pub fn find_guard_position(grid: &Vec<Vec<char>>) -> (usize, usize) {
   let m = grid.len();
   let n = grid[0].len();
   let mut x = 0;
@@ -23,4 +25,63 @@ pub fn find_guard_position(grid: Vec<Vec<char>>) -> (usize, usize) {
     panic!("No guard found in grid");
   }
   (x, y)
+}
+
+pub fn get_guard_path(grid: &Vec<Vec<char>>, m: usize, n: usize, guard_x: usize, guard_y: usize) -> (i32, Vec<Vec<char>>) {
+  // Guard always starts by going up
+  const DIRECTIONS_X: [i32; 4] = [-1, 0, 1, 0];
+  const DIRECTIONS_Y: [i32; 4] = [0, 1, 0, -1];
+  let mut position_seen = HashSet::new();
+  let mut dir_idx = 0;
+  let mut next_x = guard_x;
+  let mut next_y = guard_y;
+  let mut result = 0;
+  let mut guard_grid = grid.clone().iter().map(|x| x.clone()).collect::<Vec<Vec<char>>>();
+  while is_inside_coord(next_x as i32, next_y as i32, m, n) {
+    let next_cell = grid[next_x][next_y];
+    if next_cell == '#' {
+      next_x = ((next_x as i32) - DIRECTIONS_X[dir_idx]) as usize;
+      next_y = ((next_y as i32) - DIRECTIONS_Y[dir_idx]) as usize;
+      dir_idx += 1;
+      if dir_idx == 4 {
+        dir_idx = 0;
+      }
+    } else {
+      if !position_seen.contains(&(next_x, next_y)) {
+        result += 1;
+        guard_grid[next_x][next_y] = 'X';
+        position_seen.insert((next_x, next_y));
+      }
+    }
+    next_x = ((next_x as i32) + DIRECTIONS_X[dir_idx]) as usize;
+    next_y = ((next_y as i32) + DIRECTIONS_Y[dir_idx]) as usize;
+  }
+  (result, guard_grid)
+}
+
+pub fn is_guard_on_loop(grid: &Vec<Vec<char>>, m: usize, n: usize, guard_x: usize, guard_y: usize) -> bool {
+  // Guard always starts by going up
+  const DIRECTIONS_X: [i32; 4] = [-1, 0, 1, 0];
+  const DIRECTIONS_Y: [i32; 4] = [0, 1, 0, -1];
+  let mut position_seen = HashSet::new();
+  let mut dir_idx = 0;
+  let mut next_x = guard_x;
+  let mut next_y = guard_y;
+  while is_inside_coord(next_x as i32, next_y as i32, m, n) {
+    let next_cell = grid[next_x][next_y];
+    if next_cell == '#' {
+      next_x = ((next_x as i32) - DIRECTIONS_X[dir_idx]) as usize;
+      next_y = ((next_y as i32) - DIRECTIONS_Y[dir_idx]) as usize;
+      dir_idx += 1;
+      if dir_idx == 4 {
+        dir_idx = 0;
+      }
+    } else if position_seen.contains(&(next_x, next_y, dir_idx)) {
+      return true;
+    }
+    position_seen.insert((next_x, next_y, dir_idx));
+    next_x = ((next_x as i32) + DIRECTIONS_X[dir_idx]) as usize;
+    next_y = ((next_y as i32) + DIRECTIONS_Y[dir_idx]) as usize;
+  }
+  return false;
 }
--- packages/day-6/src/solution.rs
+++ packages/day-6/src/solution.rs
@@ -1,51 +1,34 @@
-use std::collections::HashSet;
-
-use crate::functions::{find_guard_position,is_inside_coord};
+use crate::functions::{find_guard_position, get_guard_path, is_guard_on_loop};
 
 pub fn part_1(grid: Vec<Vec<char>>) -> i32 {
-  let (guard_x, guard_y) = find_guard_position(grid.clone());
   let m = grid.len();
   let n = grid[0].len();
-  // Guard always starts by going up
-  const DIRECTIONS_X: [i32; 4] = [-1, 0, 1, 0];
-  const DIRECTIONS_Y: [i32; 4] = [0, 1, 0, -1];
-  let mut position_seen = HashSet::new();
-  let mut dir_idx = 0;
-  let mut next_x = guard_x;
-  let mut next_y = guard_y;
-  let mut result = 0;
-  let mut grad = grid.clone().iter().map(|x| x.clone()).collect::<Vec<Vec<char>>>();
-  while is_inside_coord(next_x as i32, next_y as i32, m, n) {
-    println!("dir_idx: {}, next_x: {}, next_y: {}, cell: {:?}, result: {}", dir_idx, next_x, next_y, grid[next_x][next_y], result);
-    let next_cell = grid[next_x][next_y];
-    if next_cell == '#' {
-      next_x = ((next_x as i32) - DIRECTIONS_X[dir_idx]) as usize;
-      next_y = ((next_y as i32) - DIRECTIONS_Y[dir_idx]) as usize;
-      dir_idx += 1;
-      if dir_idx == 4 {
-        dir_idx = 0;
-      }
-    } else {
-      if !position_seen.contains(&(next_x, next_y)) {
-        result += 1;
-        grad[next_x][next_y] = result.clone().to_string().chars().last().unwrap();
-        position_seen.insert((next_x, next_y));
-      }
-    }
-    next_x = ((next_x as i32) + DIRECTIONS_X[dir_idx]) as usize;
-    next_y = ((next_y as i32) + DIRECTIONS_Y[dir_idx]) as usize;
-  }
-  for graad in grad {
-    println!("{:?}", graad.iter().collect::<String>());
-  }
-  result
+  let (guard_x, guard_y) = find_guard_position(&grid);
+  let (number_of_positions, _) = get_guard_path(&grid, m, n, guard_x, guard_y);
+  number_of_positions
 }
 
 pub fn part_2(grid: Vec<Vec<char>>) -> i32 {
-  let mut result: i32 = 0;
-  // for line in grid {
-  //   println!("{:?}", line);
-  // }
+  let m = grid.len();
+  let n = grid[0].len();
+  let (guard_x, guard_y) = find_guard_position(&grid);
+  let (_, guard_grid) = get_guard_path(&grid, m, n, guard_x, guard_y);
+  let mut crate_on_path = guard_grid.clone().iter().map(|x| x.clone()).collect::<Vec<Vec<char>>>();
+  let mut result = 0;
+  for i in 0..m {
+    for j in 0..n {
+      if guard_grid[i][j] != 'X' {
+        continue;
+      }
+      // let start = std::time::Instant::now();
+      crate_on_path[i][j] = '#';
+      if is_guard_on_loop(&crate_on_path, m, n, guard_x, guard_y) {
+        result += 1;
+      }
+      crate_on_path[i][j] = 'X';
+      // println!("i: {}, j: {}, elapsed: {:?}", i, j, start.elapsed());
+    }
+  }
   result
 }
 
